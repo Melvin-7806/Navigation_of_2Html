a59f4e9f8d9c44740762c66424e516ac3ccac7ba
Melvin-7806 <127192441+Melvin-7806@users.noreply.github.com>
2023-05-26 14:21:49 +0530

    Add files via upload

---
+++ project19/app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def hi(request):
+    return render (request,'hi.html')
+
+
+def bi(request):
+    return render(request,'bi.html')
